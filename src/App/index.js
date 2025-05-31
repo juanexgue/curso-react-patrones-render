caa9cb752d1dae3b311da8022b26ef46019a6f54
Juan Guevara M. <juanexgue@gmail.com>
2025-05-31 15:40:26 -0500

    Ejercio de composicion de componentes

--- src/App/index.js
+++ src/App/index.js
@@ -1,8 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { TodoProvider } from '../TodoContext';
 import { AppUI } from './AppUI';
 
 
+function App() {
+
+  const [state, setState] = useState('');
+  return (
+    <React.Fragment>
+      <TodoHeader />
+      <TodoList />
+    </React.Fragment>
+  );
+}
+
+
+function TodoHeader() {
+  return (
+    <React.Fragment>
+      <TodoCounter />
+      <TodoSearch />
+    </React.Fragment>
+  );
+}
+
+
+function TodoList() {
+  return (
+    <React.Fragment>
+      <TodoItem />
+    </React.Fragment>
+  );
+}
+
+function TodoCounter() {
+  return <p>TodoCounter</p>;
+}
+
+function TodoSearch() {
+  return <p>TodoSearch</p>;
+}
+function TodoItem() {
+  return <p>TodoItem</p>;
+}
+
+
+/*
 function App() {
   return (
     <TodoProvider>
@@ -10,5 +53,5 @@ function App() {
     </TodoProvider>
   );
 }
-
+*/
 export default App;
